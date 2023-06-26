6e5cd7ecb503a5c2351610ff1128b50a76807d2f
tia987 <mlucca@ethz.ch>
2023-06-26 10:08:33 +0200

    Finished 9-6

--- homeworks/BoundaryWave/mysolution/boundarywave.h
+++ homeworks/BoundaryWave/mysolution/boundarywave.h
@@ -35,41 +35,58 @@ lf::assemble::COOMatrix<double> buildA(
 /* SAM_LISTING_BEGIN_7 */
 template <typename FUNCTOR_U, typename FUNCTOR_V>
 std::pair<Eigen::VectorXd, Eigen::VectorXd> interpolateInitialData(
-    std::shared_ptr<lf::uscalfe::UniformScalarFESpace<double>> fe_space_p,
-    FUNCTOR_U &&u0, FUNCTOR_V &&v0) {
-  Eigen::VectorXd dof_vector_u0, dof_vector_v0;
-
-  // Generate Lehrfem++ mesh functions out of the functors
-  //====================
-  // Your code goes here
-  //====================
-
-  std::pair<Eigen::VectorXd, Eigen::VectorXd> initialData =
-      std::make_pair(dof_vector_u0, dof_vector_v0);
-  return initialData;
+  std::shared_ptr<lf::uscalfe::UniformScalarFESpace<double>> fe_space_p,
+  FUNCTOR_U &&u0, FUNCTOR_V &&v0) {
+    Eigen::VectorXd dof_vector_u0, dof_vector_v0;
+    
+    // Generate Lehrfem++ mesh functions out of the functors
+    //====================
+		auto mf_u0 = [&u0](Eigen::VectorXd x){
+				return u0(x);
+		};
+		auto func_u0 = lf::mesh::utils::MeshFunctionGlobal(mf_u0);
+    dof_vector_u0 = lf::fe::NodalProjection(*fe_space_p,func_u0);
+    
+		auto mf_v0 = [&v0](Eigen::VectorXd x){
+				return v0(x);
+		};
+		auto func_v0 = lf::mesh::utils::MeshFunctionGlobal(mf_v0);
+    dof_vector_v0 = lf::fe::NodalProjection(*fe_space_p,func_v0);
+    //====================
+    
+    std::pair<Eigen::VectorXd, Eigen::VectorXd> initialData =
+        std::make_pair(dof_vector_u0, dof_vector_v0);
+    return initialData;
 }
 /* SAM_LISTING_END_7 */
 
 /* SAM_LISTING_BEGIN_8 */
 template <typename FUNCTOR_U, typename FUNCTOR_V>
 Eigen::VectorXd solveBoundaryWave(
-    const std::shared_ptr<lf::uscalfe::FeSpaceLagrangeO1<double>> &fe_space_p,
-    FUNCTOR_U &&u0, FUNCTOR_V &&v0, double T, unsigned int N) {
-  Eigen::VectorXd bdyWaveSol;
+  const std::shared_ptr<lf::uscalfe::FeSpaceLagrangeO1<double>> &fe_space_p,
+  FUNCTOR_U &&u0,
+	FUNCTOR_V &&v0,
+	double T,
+	unsigned int N) {
+  	Eigen::VectorXd bdyWaveSol;
 
-  double step_size = T / N;
-  // Obtain initial data
-  std::pair<Eigen::VectorXd, Eigen::VectorXd> initialData =
-      interpolateInitialData<std::function<double(Eigen::Vector2d)>,
-                             std::function<double(Eigen::Vector2d)>>(
-          fe_space_p, std::move(u0), std::move(v0));
-  // Obtain Galerkin matrices
-  lf::assemble::COOMatrix<double> M = buildM(fe_space_p);
-  lf::assemble::COOMatrix<double> A = buildA(fe_space_p);
-  //====================
-  // Your code goes here
-  //====================
-  return bdyWaveSol;
+  	double step_size = T / N;
+  	// Obtain initial data
+  	std::pair<Eigen::VectorXd, Eigen::VectorXd> initialData =
+  	    interpolateInitialData<std::function<double(Eigen::Vector2d)>,
+  	                           std::function<double(Eigen::Vector2d)>>(
+  	        fe_space_p, std::move(u0), std::move(v0));
+  	// Obtain Galerkin matrices
+  	lf::assemble::COOMatrix<double> M = buildM(fe_space_p);
+  	lf::assemble::COOMatrix<double> A = buildA(fe_space_p);
+  	//====================
+		double tau = N/T;
+		for(unsigned i = 2; i < N; i++){
+				;
+		}
+		bdyWaveSol = ;
+  	//====================
+  	return bdyWaveSol;
 };
 /* SAM_LISTING_END_8 */
 
