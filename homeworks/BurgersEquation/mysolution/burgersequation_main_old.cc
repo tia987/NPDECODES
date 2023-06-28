f561337feb8953083c160e8bd7a82be2d7a3e291
tia987 <mlucca@ethz.ch>
2023-06-28 09:43:58 +0200

    Initialized 11-1

---
+++ homeworks/BurgersEquation/mysolution/burgersequation_main_old.cc
@@ -0,0 +1,51 @@
+/**
+ * @file burgersequation_main.cc
+ * @brief NPDE homework BurgersEquation code
+ * @author Oliver Rietmann
+ * @date 15.04.2019
+ * @copyright Developed at ETH Zurich
+ */
+
+#include <Eigen/Core>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+#include "burgersequation.h"
+
+int main() {
+  /* SAM_LISTING_BEGIN_1 */
+  const unsigned int N = 100;
+  Eigen::VectorXd x = Eigen::VectorXd::LinSpaced(N + 1, -1.0, 4.0);
+  Eigen::VectorXd mu03 = BurgersEquation::solveBurgersGodunov(0.3, N);
+  Eigen::VectorXd mu30 = BurgersEquation::solveBurgersGodunov(3.0, N);
+
+  // Write the solutions to a file that can be used for plotting.
+  //====================
+  std::ofstream solution_file;
+  solution_file.open("solution.csv");
+  solution_file << x.transpose().format(BurgersEquation::CSVFormat)
+                << std::endl;
+  solution_file << mu03.transpose().format(BurgersEquation::CSVFormat)
+                << std::endl;
+  solution_file << mu30.transpose().format(BurgersEquation::CSVFormat)
+                << std::endl;
+  solution_file.close();
+  std::cout << "Generated " CURRENT_BINARY_DIR "/solution.csv" << std::endl;
+  std::system("python3 " CURRENT_SOURCE_DIR
+              "/plot_solution.py " CURRENT_BINARY_DIR
+              "/solution.csv " CURRENT_BINARY_DIR "/solution.eps");
+  //====================
+  /* SAM_LISTING_END_1 */
+
+  /* SAM_LISTING_BEGIN_2 */
+  Eigen::Matrix<double, 3, 4> result = BurgersEquation::numexpBurgersGodunov();
+
+  // Write the result to a file that can be used for plotting.
+  //====================
+  // Your code goes here
+  //====================
+  /* SAM_LISTING_END_2 */
+
+  return 0;
+}
---
+++ homeworks/BurgersEquation/mysolution/burgersequation_old.cc
@@ -0,0 +1,87 @@
+/**
+ * @file burgersequation.cc
+ * @brief NPDE homework BurgersEquation code
+ * @author Oliver Rietmann
+ * @date 15.04.2019
+ * @copyright Developed at ETH Zurich
+ */
+
+#include "burgersequation.h"
+
+#include <Eigen/Core>
+#include <cmath>
+
+namespace BurgersEquation {
+/* SAM_LISTING_BEGIN_1 */
+constexpr double PI = 3.14159265358979323846;
+
+double Square(double x) { return x * x; }
+
+double f(double x) { return 2.0 / 3.0 * std::sqrt(x * x * x); }
+
+Eigen::VectorXd solveBurgersGodunov(double T, unsigned int N) {
+  double h = 5.0 / N;           // meshwidth
+  double tau = h;               // timestep = meshwidth by CFL condition
+  int m = std::round(T / tau);  // no. of timesteps
+
+  // initialize vector with initial nodal values
+  Eigen::VectorXd x = Eigen::VectorXd::LinSpaced(N + 1, -1.0, 4.0);
+  Eigen::VectorXd mu =
+    x.unaryExpr([](double x) {
+        return 0.0 <= x && x <= 1.0 ? Square(std::sin(PI * x)) : 0.0;
+      }).eval();
+
+  //====================
+  mu(0) = 0.;
+  for(unsigned j = 0; j < m; j++){
+    for(unsigned i = N; i > 0; i--){
+      mu(i) -= tau/h*(f(mu(i))-f(mu(i-1)));
+    }
+  }
+  //====================
+
+  return mu;
+}
+/* SAM_LISTING_END_1 */
+
+/**
+ * @brief Converts a large vector on  a grid to a smaller vector correponding to
+ * a sub-grid.
+ *
+ * @param mu vector of function values on a spacial grid of size N_large
+ * @param N divides the size N_large of mu
+ * @return a vector mu_sub of size N, that represents mu on a sub-grid of size N
+ */
+/* SAM_LISTING_BEGIN_2 */
+Eigen::VectorXd reduce(const Eigen::VectorXd &mu, unsigned int N) {
+  Eigen::VectorXd mu_sub(N + 1);
+  int fraction = mu.size() / N;
+  for (int j = 0; j < N + 1; ++j) {
+    mu_sub(j) = mu(j * fraction);
+  }
+  return mu_sub;
+}
+
+Eigen::Matrix<double, 3, 4> numexpBurgersGodunov() {
+  const unsigned int N_large = 3200;
+  Eigen::Vector2d T{0.3, 3.0};
+  Eigen::Vector4i N{5 * 10, 5 * 20, 5 * 40, 5 * 80};
+  Eigen::Vector4d h;
+  for (int i = 0; i < 4; ++i) h(i) = 5.0 / N(i);
+
+  Eigen::Matrix<double, 3, 4> result;
+  result.row(0) = h.transpose();
+
+  //====================
+  for(unsigned i = 0; i < 2; i++){
+    reduce();
+  }
+  
+
+  //====================
+
+  return result;
+}
+/* SAM_LISTING_END_2 */
+
+}  // namespace BurgersEquation
