ac6d4c4f8d2981fa056a5fbb80c77a1a03436f5f
tia987 <mlucca@ethz.ch>
2023-06-28 11:11:13 +0200

    Finished 11-1

--- homeworks/BurgersEquation/mysolution/burgersequation.cc
+++ homeworks/BurgersEquation/mysolution/burgersequation.cc
@@ -20,27 +20,31 @@ double Square(double x) { return x * x; }
 double f(double x) { return 2.0 / 3.0 * std::sqrt(x * x * x); }
 
 Eigen::VectorXd solveBurgersGodunov(double T, unsigned int N) {
-  double h = 5.0 / N;           // meshwidth
-  double tau = h;               // timestep = meshwidth by CFL condition
-  int m = std::round(T / tau);  // no. of timesteps
+    double h = 5.0 / N;           // meshwidth
+    double tau = h;               // timestep = meshwidth by CFL condition
+    int m = std::round(T / tau);  // no. of timesteps
 
-  // initialize vector with initial nodal values
-  Eigen::VectorXd x = Eigen::VectorXd::LinSpaced(N + 1, -1.0, 4.0);
-  Eigen::VectorXd mu =
-    x.unaryExpr([](double x) {
-        return 0.0 <= x && x <= 1.0 ? Square(std::sin(PI * x)) : 0.0;
-      }).eval();
+    // initialize vector with initial nodal values
+    Eigen::VectorXd x = Eigen::VectorXd::LinSpaced(N + 1, -1.0, 4.0);
+    Eigen::VectorXd mu = x.unaryExpr(
+        [](double x) {
+            return 0.0 <= x && x <= 1.0 ? Square(std::sin(PI * x)) : 0.0;
+        }
+    ).eval();
 
-  //====================
-  mu(0) = 0.;
-  for(unsigned j = 0; j < m; j++){
-    for(unsigned i = N; i > 0; i--){
-      mu(i) -= tau/h*(f(mu(i))-f(mu(i-1)));
+    //====================
+
+    auto sin_f = [](Eigen::VectorXd x) -> double {
+        return Square(std::sin(PI * x.norm()));
+    };
+    for(unsigned j = 0; j < m; j++){
+        for(unsigned i = N; i > 0; i--){
+            mu(i) -= tau/h*(std::sqrt(mu(i)*mu(i)*mu(i))*2/3-std::sqrt(mu(i-1)*mu(i-1)*mu(i-1))*2/3);
+        }
     }
-  }
-  //====================
+    //====================
 
-  return mu;
+    return mu;
 }
 /* SAM_LISTING_END_1 */
 
@@ -63,24 +67,20 @@ Eigen::VectorXd reduce(const Eigen::VectorXd &mu, unsigned int N) {
 }
 
 Eigen::Matrix<double, 3, 4> numexpBurgersGodunov() {
-  const unsigned int N_large = 3200;
-  Eigen::Vector2d T{0.3, 3.0};
-  Eigen::Vector4i N{5 * 10, 5 * 20, 5 * 40, 5 * 80};
-  Eigen::Vector4d h;
-  for (int i = 0; i < 4; ++i) h(i) = 5.0 / N(i);
-
-  Eigen::Matrix<double, 3, 4> result;
-  result.row(0) = h.transpose();
-
-  //====================
-  for(unsigned i = 0; i < 2; i++){
-    reduce();
-  }
+    const unsigned int N_large = 3200;
+    Eigen::Vector2d T{0.3, 3.0};
+    Eigen::Vector4i N{5 * 10, 5 * 20, 5 * 40, 5 * 80};
+    Eigen::Vector4d h;
+    for (int i = 0; i < 4; ++i) h(i) = 5.0 / N(i);
   
-
-  //====================
-
-  return result;
+    Eigen::Matrix<double, 3, 4> result;
+    result.row(0) = h.transpose();
+  
+    //====================
+    
+    //====================
+  
+    return result;
 }
 /* SAM_LISTING_END_2 */
 
--- homeworks/BurgersEquation/mysolution/burgersequation_main.cc
+++ homeworks/BurgersEquation/mysolution/burgersequation_main.cc
@@ -14,38 +14,34 @@
 #include "burgersequation.h"
 
 int main() {
-  /* SAM_LISTING_BEGIN_1 */
-  const unsigned int N = 100;
-  Eigen::VectorXd x = Eigen::VectorXd::LinSpaced(N + 1, -1.0, 4.0);
-  Eigen::VectorXd mu03 = BurgersEquation::solveBurgersGodunov(0.3, N);
-  Eigen::VectorXd mu30 = BurgersEquation::solveBurgersGodunov(3.0, N);
+    /* SAM_LISTING_BEGIN_1 */
+    const unsigned int N = 100;
+    const double T = 3.;
+    Eigen::VectorXd x = Eigen::VectorXd::LinSpaced(N + 1, -1.0, 4.0);
+    Eigen::VectorXd mu03 = BurgersEquation::solveBurgersGodunov(0.3, N);
+    Eigen::VectorXd mu30 = BurgersEquation::solveBurgersGodunov(3.0, N);
 
-  // Write the solutions to a file that can be used for plotting.
-  //====================
-  std::ofstream solution_file;
-  solution_file.open("solution.csv");
-  solution_file << x.transpose().format(BurgersEquation::CSVFormat)
-                << std::endl;
-  solution_file << mu03.transpose().format(BurgersEquation::CSVFormat)
-                << std::endl;
-  solution_file << mu30.transpose().format(BurgersEquation::CSVFormat)
-                << std::endl;
-  solution_file.close();
-  std::cout << "Generated " CURRENT_BINARY_DIR "/solution.csv" << std::endl;
-  std::system("python3 " CURRENT_SOURCE_DIR
-              "/plot_solution.py " CURRENT_BINARY_DIR
-              "/solution.csv " CURRENT_BINARY_DIR "/solution.eps");
-  //====================
-  /* SAM_LISTING_END_1 */
+    // Write the solutions to a file that can be used for plotting.
+    //====================
+    std::ofstream sol;
+    sol.open("solution.csv");
+    sol << x.transpose().format(BurgersEquation::CSVFormat) << std::endl;
+    sol << mu03.transpose().format(BurgersEquation::CSVFormat) << std::endl;
+    sol << mu30.transpose().format(BurgersEquation::CSVFormat) << std::endl;
+    sol.close();
+    std::cout << "Generated " CURRENT_BINARY_DIR "/solution.csv" << std::endl;
+    std::system("python3 " CURRENT_SOURCE_DIR "/plot_solution.py " CURRENT_BINARY_DIR "/solution.csv " CURRENT_BINARY_DIR "/solution.eps");
+    //====================
+    /* SAM_LISTING_END_1 */
 
-  /* SAM_LISTING_BEGIN_2 */
-  Eigen::Matrix<double, 3, 4> result = BurgersEquation::numexpBurgersGodunov();
+    /* SAM_LISTING_BEGIN_2 */
+    Eigen::Matrix<double, 3, 4> result = BurgersEquation::numexpBurgersGodunov();
 
-  // Write the result to a file that can be used for plotting.
-  //====================
-  // Your code goes here
-  //====================
-  /* SAM_LISTING_END_2 */
+    // Write the result to a file that can be used for plotting.
+    //====================
+    // Your code goes here
+    //====================
+    /* SAM_LISTING_END_2 */
 
-  return 0;
+    return 0;
 }
