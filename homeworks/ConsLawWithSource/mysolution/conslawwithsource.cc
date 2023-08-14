5ad6fb93e3598240c7e7d7baa7fef02c185ba2af
tia987 <mlucca@ethz.ch>
2023-08-14 09:06:49 +0200

    Update 14/08/23

--- homeworks/ConsLawWithSource/mysolution/conslawwithsource.cc
+++ homeworks/ConsLawWithSource/mysolution/conslawwithsource.cc
@@ -14,22 +14,23 @@ namespace ConsLawWithSource {
 
 /* SAM_LISTING_BEGIN_1 */
 double godnfn(double v, double w) {
-    auto f = [](double u) { return std::exp(u) - u; };
+  auto f = [](double u) { return std::exp(u) - u; };
 
-    //====================
-    // Your code goes here
-    // Replace the dummy return value below
-    auto fd = [](double u) { return std::exp(u) - 1; };
-    if (v < w) {
-        if (0.0 < v)
-            return f(v); 
-        if (w < 0.0)
-            return f(w) ;
-        return 1.0; //=f(0.0)
-    } else {
-        return f(w) < f(v) ? f(v) : f(w);
-    }
-    //====================    
+  //====================
+  // Your code goes here
+  // Replace the dummy return value below
+  // Use 11.3.4.31
+  auto df = [](double u){return std::log(std::exp(u)-1);};
+  auto s = [f](double v, double w){return (f(v)-f(w))/(v-w);};
+  if(v == w || v > w && s(v,w) < 0 || v < w && df(w) < 0){
+      return w;
+  } else if(v > w && s(v,w) > 0 || v < w && df(v) > 0){
+      return v;
+  } else if(v < w && df(v) <= 0 && 0 <= df(w)){
+      return 1/df(0.);
+  }
+  return 0.;
+  //====================
 }
 /* SAM_LISTING_END_1 */
 
